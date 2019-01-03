a54a49b649a72859aaa1df3b1d88fa9990282f38
Bhupinder Yadav <37781596+BhupinderYadav@users.noreply.github.com>
2019-01-03 20:25:29 +0530

    Add files via upload

---
+++ Python_GUI/text_editor.py
@@ -0,0 +1,43 @@
+'''
+This file contains a sample code for a text editor to display, edit and save text data.
+'''
+import wx
+
+
+def load(event):
+    file = open(filename.GetValue())
+    contents.SetValue(file.read())
+    file.close()
+
+def save(event):
+    file = open(filename.GetValue(), 'w')
+    file.write(contents.GetValue())
+    file.close()
+
+app = wx.App()
+win = wx.Frame(None, title="IOTinterCON Editor", size=(410, 335))
+bkg = wx.Panel(win)
+
+loadButton = wx.Button(bkg, label='Open')
+loadButton.Bind(wx.EVT_BUTTON, load)
+
+saveButton = wx.Button(bkg, label='Save')
+saveButton.Bind(wx.EVT_BUTTON, save)
+
+filename = wx.TextCtrl(bkg)
+contents = wx.TextCtrl(bkg, style=wx.TE_MULTILINE | wx.HSCROLL)
+
+hbox = wx.BoxSizer()
+
+hbox.Add(filename, proportion=1, flag=wx.EXPAND)
+hbox.Add(loadButton, proportion=0, flag=wx.LEFT, border=5)
+hbox.Add(saveButton, proportion=0, flag=wx.LEFT, border=5)
+
+vbox = wx.BoxSizer(wx.VERTICAL)
+vbox.Add(hbox, proportion=0, flag=wx.EXPAND | wx.ALL, border=5)
+vbox.Add(contents, proportion=1,
+flag=wx.EXPAND | wx.LEFT | wx.BOTTOM | wx.RIGHT, border=5)
+bkg.SetSizer(vbox)
+win.Show()
+
+app.MainLoop()
